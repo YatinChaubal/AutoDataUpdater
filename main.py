b425465ea3e39c80ccd9cb6125ac13b61e966b44
YatinChaubal <yatin.chaubal@gmail.com>
2014-06-21 10:55:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,46 @@ import urllib2
 import datetime
 import zipfile
 import os
-from win32com.client import Dispatch
+import csv
+#from win32com.client import Dispatch
 
 
 
 class NseBhavcopy:
-	def __init__(self,Date=datetime.date.today()):
-	
-		curdt=Date
-		yy=curdt.year
-		mm=curdt.strftime("%b")
-		dd=curdt.day
-		
+	def __init__(self):
 
-		if dd < 10:
-			dd ="0"+str(dd)
-		url="http://www.nseindia.com/content/historical/EQUITIES/"+str(yy)+"/"+str(mm).upper()+"/cm"+str(dd)+str(mm).upper()+str(yy)+"bhav.csv.zip"
-		
-	
-		hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+		self.hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
 			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 			'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
 			'Accept-Encoding': 'none',
 			'Accept-Language': 'en-US,en;q=0.8',
 			'Connection': 'keep-alive'}
-		req = urllib2.Request(url, headers=hdr)
+
+	def converttoBhav(self,name,curdt):
+		ifile  = open(name, "rb")
+		reader = csv.reader(ifile)
+
+		f=open("bhavcop"+name,"a")
+		next(reader, None) 
+		for row in reader:
+			f.write(row[0] + "," +
+			curdt.strftime("%Y%m%d") +
+			"," + row[2] + "," + row[3] +
+			"," + row[4] + "," + row[5] +
+			"," + row[8] + "\r\n")
+		ifile.close()
+	
+	def GetBhavcopy(self,Date=datetime.date.today()):
+		curdt=Date
+		yy=curdt.year
+		mm=curdt.strftime("%b")
+		dd=curdt.day
+		
+		if dd < 10:
+			dd ="0"+str(dd)
+		url="http://www.nseindia.com/content/historical/EQUITIES/"+str(yy)+"/"+str(mm).upper()+"/cm"+str(dd)+str(mm).upper()+str(yy)+"bhav.csv.zip"
+
+		req = urllib2.Request(url, headers=self.hdr)
 
 		fileavailable=1
 		try:
@@ -36,7 +51,7 @@ class NseBhavcopy:
 			fileavailable=0
 			self.filename=""
 			self.ValidData=0
-			
+
 
 		if fileavailable == 1:
 			meta = page.info()
@@ -55,32 +70,40 @@ class NseBhavcopy:
 				status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
 				status = status + chr(8)*(len(status)+1)
 			f.close()
-		
+
 			zfile=open(file_name, 'rb')
 			z=zipfile.ZipFile(zfile)
 			for name in z.namelist():
 				outfile = open(name, 'wb')
 				outfile.write(z.read(name))
 				outfile.close()
+				self.converttoBhav(name,curdt)
 			zfile.close()
 			self.filename=name
 			self.ValidData=1
-		
-			
-
-		
-		
-def CSVToAmibroker(fname,amidb,format_file):
-
-	ab = Dispatch("Broker.Application")
-	ab.LoadDatabase(amidb)
-	ab.Import(0, fname, format_file)
-	ab.SaveDatabase()
-	print ab
-	print "Done"
-
-	
-CSVToAmibroker("C:\\NSE Auto Downloader\\cm15APR2014bhav.csv","C:\\Amibroker DB\\NSE testdb","NSE.format")
+			os.remove(name)
+			os.remove(file_name)
 
 
-	
+
+
+
+
+#def CSVToAmibroker(fname,amidb,format_file):
+
+#	ab = Dispatch("Broker.Application")
+#	ab.LoadDatabase(amidb)
+#	ab.Import(0, fname, format_file)
+#	ab.SaveDatabase()
+#	print ab
+#	print "Done"
+
+
+#CSVToAmibroker("C:\\NSE Auto Downloader\\cm15APR2014bhav.csv","C:\\Amibroker DB\\NSE testdb","NSE.format
+
+i=NseBhavcopy()
+
+for j  in xrange(1,20):
+	i.GetBhavcopy(datetime.date(2014,6,j))
+
+
